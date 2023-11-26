ff102b983fefed03b7f23eab82b1df5e0622d75f
Bence Kiss <bencekissde@gmail.com>
2023-11-26 19:03:58 +0100

    coming soon feature added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  # Route for 'Coming Soon' page
+get '/coming_soon', to: redirect('/coming_soon.html')
+
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
