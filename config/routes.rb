87a4cda149a2a74da169944e437929f4e7dd2603
Caroline Baumeister <caroline@baumeister-dv.de>
2023-11-27 17:21:52 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Route for 'Coming Soon' page
-get '/coming_soon', to: redirect('/coming_soon.html')
-
   root to: "pages#home"
+
+  resources :playlists, only: [ :new, :create, :show, :edit, :update, :destroy ] do
+    resources :playlist_songs, only: [ :new, :create, :edit, :update, :destroy]
+  end
+  # Route for 'Coming Soon' page
+  get '/coming_soon', to: redirect('/coming_soon.html')
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
   # root "posts#index"
