56e6105c8456302fb242f9228ae428643952097d
Bence Kiss <bencekissde@gmail.com>
2023-11-28 15:00:53 +0100

    added update to master branch

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,2 +1,58 @@
 class PlaylistsController < ApplicationController
+  before_action :set_playlist, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authenticate_user!, only: [ :index, :show ]
+
+  def index
+    @playlists = Playlist.all
+  end
+
+  def show
+    @playlist = Playlist.find(params[:id])
+    @playlist_songs = @playlist.playlist_songs
+    @playlist_song = PlaylistSong.new
+    @user = @playlist.user
+  end
+
+  def dashboard
+    @playlists = Playlist.where(user: current_user)
+    @songs = Song.where(user: current_user)
+    @user = current_user
+  end
+
+  def new
+    @playlist = Playlist.new
+  end
+
+  def create
+    @playlist = Playlist.new(playlist_params)
+
+    if @playlist.save
+      redirect_to playlist_path(@playlist)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @playlist.update(playlist_params)
+    redirect_to playlist_path(@playlist)
+  end
+
+  def destroy
+    @playlist.destroy
+    redirect_to playlists_path
+  end
+
+  private
+
+  def set_playlist
+    @playlist = Playlist.find(params[:id])
+  end
+
+  def playlist_params
+    params.require(:playlist).permit(:name, :description, :user_id)
+  end
 end
