a2c256666fe52c5d6043ba97beed12b05af1b069
Bence Kiss <bencekissde@gmail.com>
2023-11-29 21:48:54 +0100

    Revert "Merge pull request #19 from bentzekisz/dashboard-part2"
    This reverts commit ceac8952b6847a07c209c74777524c34bcf38674, reversing
    changes made to f82202f56b05a051760982c7e04d78e00b4319b3.

--- db/migrate/20231129161043_add_username_to_users.rb
+++ db/migrate/20231129161043_add_username_to_users.rb
@@ -1,5 +1,5 @@
 class AddUsernameToUsers < ActiveRecord::Migration[7.1]
   def change
-    # add_column :users, :username, :string
+    add_column :users, :username, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -55,7 +55,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_29_161043) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "spotify_id"
     t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
