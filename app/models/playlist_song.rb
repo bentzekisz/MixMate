fd53d363543d16a56c12aefb99f1fdf17d7192f1
Bence Kiss <bencekissde@gmail.com>
2023-12-01 16:29:16 +0100

    drag and drop working somehow

--- app/models/playlist_song.rb
+++ app/models/playlist_song.rb
@@ -1,11 +1,13 @@
 class PlaylistSong < ApplicationRecord
-  acts_as_list scope: :playlist
+  # acts_as_list scope: :playlist
   belongs_to :playlist
   belongs_to :song
 
+
   validate :max_songs_limit
   validates :position, uniqueness: { scope: :playlist_id }
-
+  scope :ordered, -> { order(position: :asc) }
+  before_create :set_default_position
   private
 
   def max_songs_limit
@@ -13,4 +15,9 @@ class PlaylistSong < ApplicationRecord
       errors.add(:playlist, 'the playlist cannot have more than 12 songs')
     end
   end
+
+  def set_default_position
+    self.position = (PlaylistSong.where(playlist_id: playlist_id).maximum(:position) || 0) + 1
+  end
+  
 end
